6dc62e66a71aa63d0ac10e207a2139f4f6a94ccb
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-08-04 20:38:02 -0300

    hello, world

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header'
+
+// conceito de propriedades
+function App() {
+  return (
+  <>
+    <Header title="O BRABO TEM NOME"/>
+    <Header title="O PAI TA ONLINE"/>
+  </>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Header(props) {
+  return (
+      <>
+        <header>{props.title}</header>
+      </>
+  );
+}
+
+export default Header;
